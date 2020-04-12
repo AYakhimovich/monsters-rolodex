83e36eec01f20d3ef822f3420ea90cabf4812f33
AYakhimovich <a.i.yakhimovich@gmail.com>
2020-04-12 21:30:53 +0400

    Updates

---
+++ precache-manifest.68a8373278622ab19e0b7601c1485e28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e22884d8bb85dd845608ce0f67d1de8e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3f737d4c22ef1f38ac63",
+    "url": "/monsters-rolodex/static/css/main.07d4dce3.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f737d4c22ef1f38ac63",
+    "url": "/monsters-rolodex/static/js/main.3b73e015.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
